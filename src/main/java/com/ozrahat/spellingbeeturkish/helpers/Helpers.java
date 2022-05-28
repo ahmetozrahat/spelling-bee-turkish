424f26ca01f976c1531ac10db58ee477813878cb
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2022-05-28 07:04:03 +0300

    added pangram checkher method.

--- src/main/java/com/ozrahat/spellingbeeturkish/helpers/Helpers.java
+++ src/main/java/com/ozrahat/spellingbeeturkish/helpers/Helpers.java
@@ -1,5 +1,7 @@
 package com.ozrahat.spellingbeeturkish.helpers;
 
+import com.ozrahat.spellingbeeturkish.model.GameModel;
+
 public class Helpers {
 
     /**
@@ -12,5 +14,25 @@ public class Helpers {
         StringBuilder charSequence = new StringBuilder(word);
         charSequence.deleteCharAt(charSequence.length() - 1);
         return charSequence.toString();
+    }
+
+    /**
+     * Method to indicate that whether the given word is pangram or not.
+     *
+     * @param word The word.
+     * @param gameModel GameModel object.
+     * @return true or false.
+     */
+    public static boolean isPangram(String word, GameModel gameModel) {
+        if (word.length() == gameModel.getLetters().size() + 1) {
+            for (int i = 0; i < word.length(); i++) {
+                String formattedChar = String.valueOf(word.charAt(i)).toUpperCase();
+                if (!gameModel.getLetters().contains(formattedChar) && !gameModel.getCenterLetter().equals(formattedChar))
+                    return false;
+            }
+            return true;
+        }else {
+            return false;
+        }
     }
 }
