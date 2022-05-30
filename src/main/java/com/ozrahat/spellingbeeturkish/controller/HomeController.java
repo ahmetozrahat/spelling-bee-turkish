99f1017c227a18b850a5db6165c30437977c3925
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2022-05-30 20:15:37 +0300

    pushView method moved to Helpers class.

--- src/main/java/com/ozrahat/spellingbeeturkish/controller/HomeController.java
+++ src/main/java/com/ozrahat/spellingbeeturkish/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.ozrahat.spellingbeeturkish.controller;
 
 import com.ozrahat.spellingbeeturkish.Main;
+import com.ozrahat.spellingbeeturkish.helpers.Helpers;
 import com.ozrahat.spellingbeeturkish.model.GameModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
 
 public class HomeController {
 
@@ -34,7 +32,7 @@ public class HomeController {
             GameController gameController = fxmlLoader.getController();
             gameController.setModel(gameModel);
 
-            pushView(root, autoGenerateButton);
+            Helpers.pushView(root, autoGenerateButton);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Failed to open game window.");
@@ -47,24 +45,10 @@ public class HomeController {
         try {
             Parent root = fxmlLoader.load();
 
-            pushView(root, manualGenerateButton);
+            Helpers.pushView(root, manualGenerateButton);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Failed to open game window.");
         }
-    }
-
-    private void pushView(Parent root, Button manualGenerateButton) {
-        Scene scene = new Scene(root, 960, 540);
-        Stage stage = new Stage();
-
-        Image image = new Image(Main.class.getResourceAsStream("bee.png"));
-        stage.getIcons().add(image);
-
-        stage.setTitle("Spelling Bee Turkish");
-        stage.setScene(scene);
-        stage.show();
-
-        manualGenerateButton.getScene().getWindow().hide();
     }
 }
